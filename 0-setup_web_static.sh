052abd19067ec64eab61b2dfdb4a02f6ac01cb0f
kennyChukwuebuka <kenkinado@gmail>
2024-01-10 23:31:18 +0100

    added setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,43 @@
 #!/usr/bin/env bash
-# script that sets up your web servers for the deployment of web_static. It must
+# script that sets up your web servers for the deployment of web_static
 
-if [ ! -d data ]; then
-    mkdir data
-fi
-if [ ! -d data/web_static ]; then
-    mkdir data/web_static
-fi
-if [ ! -d data/web_static/releases ]; then
-    mkdir data/web_static/releases
-fi
-if [ ! -d data/web_static/shared ]; then
-    mkdir data/web_static/shared
-fi
-if [ ! -d data/web_static/releases/test ]; then
-    mkdir data/web_static/releases/test
-fi
-if [ ! -d data/web_static/releases/test/index.html ]; then
-    touch data/web_static/releases/test/index.html
-    echo "Holberton School" > data/web_static/releases/test/index.html
-fi
+#install nginx
+apt-get update
+apt-get install -y nginx
+
+#set up a folder
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#set up permissions
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+#set up nginx config file
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+#restart nginx
+service nginx restart
