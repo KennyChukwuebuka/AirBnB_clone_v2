a5632766c044f3c10ff2a9143cdbfbaa3eecdd41
kennyChukwuebuka <kenkinado@gmail>
2024-01-31 22:06:40 +0100

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,31 +15,28 @@ Routes:
 
 """
 
-from flask import Flask
-from flask import render_template
 from models import storage
-
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
-    from models import storage
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display an HTML page: (inside the tag BODY)
-    H1 tag: “States”
-    UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z)
-    LI tag: description of one State: <state.id>: <B><state.name></B>
-    You must use the option strict_slashes=False in your route definition
-    """
-    states = storage.all('State')
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
